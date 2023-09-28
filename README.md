84be9c4d0e851a3bcfca3c6e6fe4407ebd578031
Caduzinhok <carlos_fire1@outlook.com>
2023-09-28 20:40:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### First site developed on SCRIMBA course.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar"
+import MainContent from "./components/MainContent"
+import "./App.css"
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <MainContent />
+        </div>
+    )
+}
